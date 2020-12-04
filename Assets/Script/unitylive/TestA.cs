faf034fae805bb0fa574392be91ffa7ee57c56a7
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-12-04 21:42:52 +0900

    コミット忘れ

---
+++ Assets/Script/unitylive/TestA.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestA : MonoBehaviour
+{
+    // アクセス修飾子
+    public int m_iTestAInt;
+    public int m_iRecieveTestBInt;
+}
---
+++ Assets/Script/unitylive/TestC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestC : MonoBehaviour
+{
+    public TestA instance_a;
+    public TestB instance_b;
+
+    void Start()
+    {
+        Debug.Log(instance_a.m_iTestAInt);
+        Debug.Log(instance_b.m_iTestBInt);
+    }
+}
